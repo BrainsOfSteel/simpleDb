edbf343db44c6f186c8b51eec5b98fb54dfecea4
BrainsOfSteel <milindsah1234@gmail.com>
2023-02-18 11:16:41 +0530

    dummy commit

--- src/main/java/com/simple/database/database/master/engine/SnapshotManager.java
+++ src/main/java/com/simple/database/database/master/engine/SnapshotManager.java
@@ -71,6 +71,7 @@ public class SnapshotManager implements InitializingBean {
             ///stop the database engine
             executorService.shutdown();
             System.out.println("Snapshot manager shutdown completed");
+            //
         }
     }
 
